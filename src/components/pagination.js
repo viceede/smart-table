32df3c6bab53799ff36118a9f7656efbb6f197d4
viceede <d4rell1@yandex.ru>
2026-06-08 02:32:07 +0300

    начал работу над пагинацией

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,17 +2,28 @@ import {getPages} from "../lib/utils.js";
 
 export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
     // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+    const pageTemplate = pages.firstElementChild.cloneNode(true);    // в качестве шаблона берём первый элемент из контейнера со страницами
+    pages.firstElementChild.remove();                                // и удаляем его (предполагаем, что там больше ничего, как вариант, можно и всё удалить из pages)
 
     return (data, state, action) => {
         // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+        const rowsPerPage = state.rowsPerPage;                        // будем часто обращаться, чтобы короче записывать
+        const pageCount = Math.ceil(data.length / rowsPerPage);        // число страниц округляем в большую сторону
+        let page = state.page;                                        // страница переменной, потому что она может меняться при обработке действий позже
 
         // @todo: #2.6 — обработать действия
 
         // @todo: #2.4 — получить список видимых страниц и вывести их
+        const visiblePages = getPages(page, pageCount, 5);                // Получим массив страниц, которые нужно показать, выводим только 5 страниц
+        pages.replaceChildren(...visiblePages.map(pageNumber => {        // перебираем их и создаём для них кнопку
+            const el = pageTemplate.cloneNode(true);                    // клонируем шаблон, который запомнили ранее
+            return createPage(el, pageNumber, pageNumber === page);        // вызываем колбэк из настроек, чтобы заполнить кнопку данными
+        }))
 
         // @todo: #2.5 — обновить статус пагинации
 
         // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        return data.slice(0, 10);
+        const skip = (page - 1) * rowsPerPage;            // сколько строк нужно пропустить
+        return data.slice(skip, skip + rowsPerPage);    // получаем нужную часть строк (заменяем имеющийся return)
     }
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
+import {initPagination} from "./components/pagination.js"
 // @todo: подключение
 
 
@@ -19,9 +20,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,7 +38,7 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-
+    result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
@@ -42,11 +47,21 @@ const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
     before: [],
-    after: []
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
-
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
