ad0ec8352adf3f1860365bbfbedc7d5b30a0b43c
viceede <d4rell1@yandex.ru>
2026-06-27 02:02:12 +0300

    переделал пагинацию

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -5,11 +5,11 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
     const pageTemplate = pages.firstElementChild.cloneNode(true);    // в качестве шаблона берём первый элемент из контейнера со страницами
     pages.firstElementChild.remove();                                // и удаляем его (предполагаем, что там больше ничего, как вариант, можно и всё удалить из pages)
 
-    return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
-        const rowsPerPage = state.rowsPerPage;                        // будем часто обращаться, чтобы короче записывать
-        const pageCount = Math.ceil(data.length / rowsPerPage);        // число страниц округляем в большую сторону
-        let page = state.page;                                        // страница переменной, потому что она может меняться при обработке действий позже
+    let pageCount
+
+    const applyPagination = (query, state, action) => {
+        const limit = state.rowsPerPage;
+        let page = state.page;
 
         // @todo: #2.6 — обработать действия
         if (action) switch(action.name) {
@@ -19,6 +19,12 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
             case 'last' : page = pageCount; break;
         }
 
+        return Object.assign({}, query, {limit, page});
+    }
+
+    const updatePagination = (total, {page, limit}) => {
+        pageCount = Math.ceil(total / limit);
+
         // @todo: #2.4 — получить список видимых страниц и вывести их
         const visiblePages = getPages(page, pageCount, 5);                // Получим массив страниц, которые нужно показать, выводим только 5 страниц
         pages.replaceChildren(...visiblePages.map(pageNumber => {        // перебираем их и создаём для них кнопку
@@ -27,12 +33,13 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
         }))
 
         // @todo: #2.5 — обновить статус пагинации
-        fromRow.textContent = (page - 1) * rowsPerPage + 1;                    // С какой строки выводим
-        toRow.textContent = Math.min((page * rowsPerPage), data.length);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
-        totalRows.textContent = data.length;                                // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
-
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        const skip = (page - 1) * rowsPerPage;            // сколько строк нужно пропустить
-        return data.slice(skip, skip + rowsPerPage);    // получаем нужную часть строк (заменяем имеющийся return)
+        fromRow.textContent = (page - 1) * limit + 1;                    // С какой строки выводим
+        toRow.textContent = Math.min((page * limit), total);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+        totalRows.textContent = total;
     }
+
+    return {
+        updatePagination,
+        applyPagination
+    };
 }
--- src/main.js
+++ src/main.js
@@ -33,6 +33,8 @@ function collectState() {
     };
 }
 
+
+
 /**
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
@@ -44,7 +46,9 @@ async function render(action) {
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action); */
+    query = applyPagination(query, state, action);
     const {total, items} = await api.getRecords(query);
+    updatePagination(total, query);
     sampleTable.render(items);
     
 }
@@ -69,7 +73,7 @@ const applySorting = initSorting([        // Нам нужно передать 
     sampleTable.header.elements.sortByTotal
 ]);
 
-const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
         const input = el.querySelector('input');
